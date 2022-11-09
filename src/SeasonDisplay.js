c3c7ad3e0f7901a27a253b0b46ed274a0bc9820a
Sitt Hmu Eain <yoopajdoto@gmail.com>
2022-11-10 03:20:59 +0630

    add icons and styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from "react";
+import './SeasonDisplay.css'
 
+const SeasonDisplay = (props) =>{
+    const season = getSeason(props.lat,new Date().getMonth());
+    const {text,iconName} = seasonConfig[season] ;
+    return (
+        <div className={`season-display ${season}`}>
+            <i className ={`icon-left massive ${iconName} icon`}/>
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`}/>
+        </div>
+    )
+} 
 const seasonConfig = {
     summer : {
         text: "let's hit the beach",
@@ -17,15 +29,5 @@ const getSeason = (lat,month) => {
        return lat > 0 ? 'winter' : 'summer';
     }
 };
-const SeasonDisplay = (props) =>{
-    const season = getSeason(props.lat,new Date().getMonth());
-    const {text,iconName} = seasonConfig[season] ;
-    return (
-        <div>
-            <i className ={`massive ${iconName} icon`}/>
-            <h1>{text}</h1>
-            <i className={`massive ${iconName} icon`}/>
-        </div>
-    )
-} 
+
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-
+import Spinner from "./Spinner";
 
 class App extends React.Component{        
         state = {lat:null,errorMessage: ''};
@@ -13,15 +13,21 @@ class App extends React.Component{
             err => this.setState({errorMessage: err.message})
         )
     };
-    
-    render(){
+
+    renderContent (){
         if(this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>
         }
         if(!this.state.errorMessage && this.state.lat){
             return <SeasonDisplay lat= {this.state.lat}/>
         }
-        return <div>Loading!</div>;
+        return <Spinner message="Please Accept locationn request"/>;
+    };
+    
+    render(){
+       return(
+        <div className="border red">{this.renderContent()}</div>
+       )
     } 
 }
 
